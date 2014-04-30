cbf8c7f63fe43ddfa989203ebf40eee42a9cdc37
Lenscak José Francisco <jose.lenscak@globant.com>
2014-04-30 18:30:57 -0300

    Actividad 3.6 Juego de Rol.

--- unidad_3/actividad-3-5.cpp
+++ unidad_3/actividad-3-5.cpp
@@ -5,25 +5,81 @@
 #include <ctime>
 using namespace std;
 
+int lanzarDados(int tipo, int cantidad, int base){
+	int resultado = 0;
+	
+	for(int i = 0; i < cantidad; i++){
+		resultado += rand() % tipo + 1;
+	}
+	
+	return resultado + base;
+}
+
+int calcularDano(){
+	int dano = lanzarDados(6, 2, 3) - lanzarDados(4, 2, 1);
+	if(dano < 0){
+		dano = 0;
+	}
+	
+	return dano;
+}
+
+int calcularCura(){
+	
+	return lanzarDados(6, 1, 0);
+}
+
 int main(int argc, char *argv[]) {
 	srand(time(NULL));
-	char respuesta = 'S';
+	int vidaJugador = rand() % 100 + 20;
+	int vidaEnemigo = rand() % 80 + 10;
+	int puntos;
 	
-	while (respuesta == 's' || respuesta == 'S'){
-
-		int dado1 = rand()%6+1;
-		int dado2 = rand()%6+1;
-		
-		cout << "Los resultados fueron: " << dado1 << " y " << dado2 << endl;
-		if(dado1 == dado2){
-			cout << "Ambos dados dieron el mismo resultado" << endl;
-		} else {
-			cout << "Ambos dados son diferentes\n";
+	char respuesta = 'p';
+	
+	while (respuesta != 's'){
+		switch(respuesta){
+		case 'p':
+			puntos = calcularDano();
+			cout << "\nHas hecho " << puntos << " puntos de daño.\n";
+			vidaEnemigo -= puntos;
+			break;
+		case 'c':
+			puntos = calcularCura();
+			cout << "\nTe has curado " << puntos << " puntos de daño.\n";
+			vidaJugador += puntos;
+			break;
+		default:
+			cout << "\nOpción invalida.\n";
 		}
 		
-		cout << "En total ambos dados suman: " << dado1 + dado2 << endl;
-		cout << "\n¿Quieres lanzar otra vez?";
-		cin >> respuesta;
+		
+		if(vidaEnemigo > 10){
+			puntos = calcularDano();
+			cout << "Te han hecho " << puntos << " puntos de daño.\n";
+			vidaJugador -= puntos;
+		} else {
+			puntos = calcularCura();
+			cout << "El enemigo se ha curado " << puntos << " puntos de daño.\n";
+			vidaEnemigo += puntos;
+		}
+		
+		if(vidaEnemigo > 0){
+			if(vidaJugador > 0){
+				cout << "\nTu vida: " << vidaJugador << endl;
+				cout << "La vida de tu enemigo: " << vidaEnemigo << endl;
+				cout << "p => Pegar\n";
+				cout << "c => Curar\n";
+				cout << "s => Salir\n";
+				cin >> respuesta;
+			} else {
+				cout << "Tu cuerpo inerte yace en el suelo.\n";
+				respuesta = 's';
+			}
+		} else {
+			cout << "Has logrado vencer a el vil asesino.\n";
+			respuesta = 's';
+		}
 	}
 	
 	return 0;
