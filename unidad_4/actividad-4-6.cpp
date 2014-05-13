54e6e4b7c0d54826d01421859c97f0b2a57e0ced
Lenscak José Francisco <jose.lenscak@globant.com>
2014-05-13 16:58:29 -0300

    Actividad 4.6, tirada con penalización.

---
+++ unidad_4/actividad-4-6.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int dado(int caras);
+int penalizacion(int caras, int maximo);
+
+int main(int argc, char *argv[]) {
+	srand(time(NULL));
+	
+	int a, b, c;
+	cout << "Cantidad de caras del dado: ";
+	cin >> a;
+	cout << "\nPenalización: ";
+	cin >> b;
+	c = penalizacion(a, b);
+	cout << endl << "\nSalio: " << c << endl;
+	
+	return 0;
+}
+
+int dado(int caras){
+	return rand() % caras + 1;
+}
+
+int penalizacion(int caras, int maximo){
+	int tirada;
+	do{
+		tirada = dado(caras);
+		cout << tirada << endl;
+	} while(tirada >= maximo);
+	
+	return tirada;
+}
