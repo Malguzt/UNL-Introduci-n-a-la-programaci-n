d99f892b7f49a5e44118a4ad1a5bfece99a91566
Lenscak José Francisco <malguzt@gmail.com>
2014-07-19 02:40:14 -0300

    Juego de ahoracado funcionando. Podrias separarce en funciones, lo dejo para otro momento.

--- unidad_6/actividad-6-13.cpp
+++ unidad_6/actividad-6-13.cpp
@@ -12,38 +12,95 @@ void dibujarAhorcado(int);
 
 typedef char palabra[20];
 int main(int argc, char const *argv[]){
+  srand(time(NULL));
   ifstream archivo("palabras.txt");
 
   char **palabras = NULL;
   char *nuevaPalabra = new palabra;
   char **palabrasAnteriores;
-  int j = 0;
+  int totalPalabras = 0;
 
   while(!archivo.eof()){
     archivo >> nuevaPalabra;
     palabrasAnteriores = palabras;
 
-    palabras = new char* [j + 1];
+    palabras = new char* [totalPalabras + 1];
 
-    for (int i = 0; i < j; ++i){
+    for (int i = 0; i < totalPalabras; ++i){
       palabras[i] = palabrasAnteriores[i];
     }
 
-    palabras[j] = nuevaPalabra;
+    palabras[totalPalabras] = nuevaPalabra;
 
-    j++;
+    totalPalabras++;
     delete palabrasAnteriores;
     nuevaPalabra = new palabra;
   }
 
-  srand(time(NULL));
-  int x = rand()%j;
+  int errores;
+  char letra;
+  bool existe;
+  int encontradas;
+  palabra descubiertas;
+  char respuesta;
+  for (int i = 0; i < totalPalabras; ++i){
+    int x = rand()%totalPalabras;
+    errores = 0;
+    encontradas = 0;
 
-  cout << palabras[x] << endl;
-  char nada[4];
-  for (int i = 0; i < 6; ++i){
-    dibujarAhorcado(i);
-    cin >> nada;
+    for (int j = 0; j < strlen(palabras[x]); ++j){
+      descubiertas[j] = '_';
+      descubiertas[j + 1] = '\0';
+    }
+
+    while(errores < 5 && encontradas < strlen(palabras[x])){
+      dibujarAhorcado(errores);
+      gotoxy(10,3);
+      textcolor(WHITE);
+      cout << descubiertas;
+      gotoxy(1, 10);
+      letra = getch();
+
+      existe = false;
+      for (int j = 0; j < strlen(palabras[x]); ++j){
+        if(palabras[x][j] == letra && descubiertas[j] != letra){
+          descubiertas[j] = letra;
+          encontradas++;
+          existe = true;
+        }
+      }
+
+      if(!existe){
+        errores++;
+      }
+    }
+
+
+    if(errores == 5){
+      dibujarAhorcado(5);
+      gotoxy(1, 9);
+      textcolor(RED);
+      cout << "Lo lamento, has perdido. La palabra era: " << palabras[x] << endl;
+    }
+
+    if(encontradas == strlen(palabras[x])){
+      gotoxy(10,3);
+      textcolor(WHITE);
+      cout << descubiertas;
+      gotoxy(1, 9);
+      textcolor(GREEN);
+      cout << "Felicidades, has ganado\n";
+    }
+
+    cout << "¿Jugar de nuevo? s/n\n";
+    respuesta = ' ';
+    while(respuesta != 's' && respuesta != 'n'){
+      respuesta = getch();
+    }
+
+    if(respuesta == 'n'){
+      break;
+    }
   }
   
   return 0;
