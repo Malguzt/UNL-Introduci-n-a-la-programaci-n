8387aba1a0e79a8a6fc7fa87721b1519c2d34624
Lenscak José Francisco <malguzt@gmail.com>
2014-07-19 01:48:45 -0300

    Dibujando el ahoracado.

--- unidad_6/actividad-6-13.cpp
+++ unidad_6/actividad-6-13.cpp
@@ -5,9 +5,10 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <conio2.h>
 using namespace std;
 
-
+void dibujarAhorcado(int);
 
 typedef char palabra[20];
 int main(int argc, char const *argv[]){
@@ -39,6 +40,73 @@ int main(int argc, char const *argv[]){
   int x = rand()%j;
 
   cout << palabras[x] << endl;
+  char nada[4];
+  for (int i = 0; i < 6; ++i){
+    dibujarAhorcado(i);
+    cin >> nada;
+  }
   
   return 0;
+}
+
+void dibujarAhorcado(int errores){
+  clrscr();
+  gotoxy(1,1);
+  textcolor(BLUE);
+  cout << " _______" << endl;
+  cout << " |     |" << endl;
+  cout << "       |" << endl;
+  cout << "       |" << endl;
+  cout << "       |" << endl;
+  cout << "       |" << endl;
+  cout << "       |" << endl;
+  textcolor(GREEN);
+  cout << "------------" << endl;
+
+  switch(errores){
+    case 4:
+      gotoxy(2,5);
+      textcolor(BLUE);
+      cout << "/\\";
+    case 3:
+      gotoxy(1,4);
+      textcolor(YELLOW);
+      cout << "-()-";
+    case 2:
+      gotoxy(2,4);
+      textcolor(YELLOW);
+      cout << "()";
+    case 1:
+      gotoxy(2,3);
+      textcolor(BROWN);
+      cout << "O";
+      break;
+    case 5: // Dibujo especial para cuando pierda
+      gotoxy(2,3); //Cabeza
+      textcolor(BROWN);
+      cout << "O";
+
+      gotoxy(1,4); //Torso
+      textcolor(YELLOW);
+      cout << "-";
+      textcolor(RED);
+      cout << "(";
+      textcolor(YELLOW);
+      cout << ")-";
+
+      gotoxy(2,5); //Piernas
+      textcolor(RED);
+      cout << "/";
+      textcolor(BLUE);
+      cout << "\\";
+
+      gotoxy(2,6); //Sangre
+      textcolor(RED);
+      cout << ".";
+      gotoxy(2,7);
+      cout << "\"";
+      gotoxy(1,8);
+      cout << "---";
+      break;
+  }
 }
